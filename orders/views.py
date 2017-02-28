281284e43709031e82e7e0c83cd9ac26d3f29319
geopold <akimowsn17l@gmail.com>
2017-02-28 10:50:03 +0300

    first commit

---
+++ orders/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
+from django.shortcuts import render, get_object_or_404
+from .models import OrderItem, Order
+from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+import weasyprint
+
+def OrderCreate(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            cart.clear()
+            return render(request, 'orders/order/created.html', {'order': order})
+
+    form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {'cart': cart,
+                                                        'form': form})
+
+
+@staff_member_required
+def AdminOrderDetail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
+
+@staff_member_required
+def AdminOrderPDF(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename=order_{}.pdf'.format(order.id)
+    weasyprint.HTML(string=html).write_pdf(response,
+               stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/bootstrap.min.css')])
+    return response
